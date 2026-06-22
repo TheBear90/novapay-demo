a4eabbf5d9fb8d7789a4eca2fd6982efc9f32018
TheBear90 <124815595+TheBear90@users.noreply.github.com>
2026-06-22 13:54:35 -0300

    Delete [...slug].js

--- [...slug].js
+++
@@ -1,6 +0,0 @@
-const demoHandler = require('./_demo-handler');
-
-module.exports = async (req, res) => {
-  const slug = Array.isArray(req.query.slug) ? req.query.slug.join('/') : req.query.slug;
-  return demoHandler(req, res, slug);
-};
